90cde7a5d089754d78da4444b50a1b850a6eefab
Colin McDonald <mickydwow@gmail.com>
2016-03-23 16:21:25 -0700

    working it

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,13 +2,12 @@ var webpack = require('webpack');
 var path = require('path');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var buildPath = path.resolve(__dirname, 'public', 'build');
-var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
 var config = {
 
   // We change to normal source mapping
   devtool: 'source-map',
-  entry: mainPath,
+  entry: './index.js',
   output: {
     path: buildPath,
     filename: 'bundle.js'
