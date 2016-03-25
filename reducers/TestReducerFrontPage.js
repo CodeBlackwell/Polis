0dc8795aa70f58402db69d3786a88717d52f2b6b
DejonGill <dejonjgill@gmail.com>
2016-03-25 15:23:41 -0700

    Minor changes

---
+++ reducers/TestReducerFrontPage.js
@@ -0,0 +1,8 @@
+// import expect from 'expect'
+// import * as actions from '../actions/ActionsFrontPage'
+
+// describe('actions', () => {
+//   it('should return the current state', () => {
+    
+//   });
+// });
