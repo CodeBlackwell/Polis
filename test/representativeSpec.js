ae5f913bd1ef9d9aa3f086583d229eb6f4a1eb0b
Colin McDonald <mickydwow@gmail.com>
2016-03-30 18:48:58 -0700

    Rep page needs graph stuff

--- test/representativeSpec.js
+++ test/representativeSpec.js
@@ -8,21 +8,23 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import {expect} from 'chai';
 import { SELECT_REPRESENTATIVE,
+          GET_REP_INFO,
+          getRepInfo,
           setRepresentative } from '../src/actions/index'
 import representativeList from '../src/reducers/reducerProfile'
 
-
+const middlewares = [ thunk ]
+const mockStore = configureMockStore(middlewares)
 
 describe('Select Representative', () => {
 
   it('should create an action to select a representative', () => {
-    const id = '12345'
+    const rep = '12345'
     const expectedAction = {
       type: SELECT_REPRESENTATIVE,
-      id
+      rep
     }
-    console.log(setRepresentative(id) === expectedAction)
-    expect(setRepresentative(id)).to.deep.equal(expectedAction);
+    expect(setRepresentative(rep)).to.deep.equal(expectedAction);
   });
 
   it('should return the initial state', () => {
@@ -37,15 +39,34 @@ describe('Select Representative', () => {
       )
   })
 
-  it('should handle SELECT_REPRESENTATIVE')
+  it('should handle SELECT_REPRESENTATIVE', () => {
     expect(
       representativeList([], {
         type: SELECT_REPRESENTATIVE,
-        representative: 'hello'
+        rep: 'hello'
       })
     ).to.deep.equal(
       {
         representative: 'hello'
       }
     )
+  })
+
+    it('creates REP_INFO when fetching rep info is done', (done) => {
+      nock('https://en.wikipedia.org')
+        .get('/w/api.php')
+        .query(true)
+        .reply(200, { body: { info: 'data' }})
+
+        const expectedActions = [{ type: GET_REP_INFO, info: { body: {info: 'data'} }}]
+
+        const store = mockStore({ body: {} })
+
+        store.dispatch(getRepInfo())
+          .then(() => {
+            expect(store.getActions()).to.deep.equal(expectedActions)
+          })
+          .then(done)
+          .catch(done)
+    })
 });
