6afbc6e49598c1e9bc41bc3f8527e9a8d2784b6c
Colin McDonald <mickydwow@gmail.com>
2016-04-01 14:19:12 -0700

    D3 is working with test data

--- src/actions/actionContributor.js
+++ src/actions/actionContributor.js
@@ -19,54 +19,22 @@ export function setGraphType() {
 function createData() {
 	let n = 6 // number of layers
   let m = 3 // number of bars
-  // layers = [ [ { x: 0, y: 3410, y0: 0 },
-  //   { x: 1, y: 153550, y0: 0 },
-  //   { x: 2, y: 0, y0: },
-  //   { x: 3, y: 0, y0: },
-  //   { x: 4, y: 0, y0: },
-  //   { x: 5, y: 156960, y0: } ],
-
 
   let layers = [
-    [{x: 0, y: 3410, y0: 0}, {x:1, y: 8301, y0: 0}, {x: 2, y: 23152.81, y0: 0}],
-    [{x: 0, y: 153550, y0: 3410}, {x: 1, y: 50550, y0: 8301}, {x: 2, y: 377748, y0: 23152.81}],
-    [{x: 0, y: 0, y0: 153550}, {x: 1, y: 0, y0: 50550}, {x: 2, y: 0, y0: 377748}],
-    [{x: 0, y: 0, y0: 0}, {x: 1, y: 35748.35, y0: 0}, {x: 2, y: 279609.1, y0: 0}],
-    [{x: 0, y: 0, y0: 0}, {x: 1, y: 0, y0: 35748.35}, {x: 2, y: 0, y0: 279609.1}],
-    [{x: 0, y: 156960, y0: 0}, {x: 1, y: 94599.35, y0: 0}, {x: 2, y: 680509.91, y0: 0}]
+    [{x: 0, y: 3410}, {x:1, y: 8301}, {x: 2, y: 23152.81}],
+    [{x: 0, y: 153550}, {x: 1, y: 50550}, {x: 2, y: 377748}],
+    [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
+    [{x: 0, y: 0}, {x: 1, y: 35748.35}, {x: 2, y: 279609.1}],
+    [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
+    [{x: 0, y: 156960}, {x: 1, y: 94599.35}, {x: 2, y: 680509.91}]
   ]
-
   let stack = d3.layout.stack()
-  
-  function bumpLayer(n, o) {
 
-    function bump(a) {
-      let x = 1 / (.1 + Math.random()),
-          y = 2 * Math.random() - .5,
-          z = 10 / (.1 + Math.random());
-      for (let i = 0; i < n; i++) {
-        let w = (i / n - y) * z;
-        a[i] += x * Math.exp(-w * w);
-      }
-    }
-
-    let a = [], i;
-    for (i = 0; i < n; ++i) a[i] = o + o * Math.random();
-
-    //console.log('this is a', a)
-    //a are the y values
-    //they become the y0 for the next array
-    for (i = 0; i < 5; ++i) bump(a);
-    return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-  }
- // console.log('this is bumplayer', bumpLayer(m, .1))
-
-  //let layers = stack(d3.range(n).map(function() { return bumpLayer(m, .1); }))
-  console.log(layers)
+  let theseLayers = stack(layers)
   return {
     m: m,
     n: n,
-    layers: layers,
+    layers: theseLayers,
   }
 }
 
