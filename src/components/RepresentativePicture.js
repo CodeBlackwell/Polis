86ed81cd3488f6c9e032c5d51bcbcf8ef923893a
Colin McDonald <mickydwow@gmail.com>
2016-04-05 16:09:14 -0700

    Working on refactoring the routing

--- src/components/RepresentativePicture.js
+++ src/components/RepresentativePicture.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 const RepresentativePicture = ({representative, image, selectRep}) => (
 		<div className='col-md-4'>
-			<Link to={{ pathname: '/representative/' + representative.id }}><img src={image} className='img-responsive rep-img'/></Link>
+			<Link to={{ pathname: '/representatives/' + representative.id }}><img src={image} className='img-responsive rep-img'/></Link>
 			<div>{representative.person.firstname + ' ' + representative.person.lastname}</div>
 			<div>{representative.role_type_label}</div>
 		</div>
--- src/containers/RepresentativeInfo.js
+++ src/containers/RepresentativeInfo.js
@@ -1,42 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const RepresentativeInfo = ({representative}) => (
-	<div>
-		<div className='row rep-info-container'>
-				<div className='col-md-2 col-md-offset-2 rep-info'>
-					<div>Website:</div>
-					<a href={representative.website}>{representative.website}</a>
-				</div>
-			<div className='col-md-2 col-md-offset-1 rep-info'>
-					<div>Twitter:</div>
-					<a href={'https://www.twitter.com/@' + representative.person.twitterid}>@{representative.person.twitterid}</a>
+export default class RepresentativeInfo extends Component {
+	render() {
+		const { params, representatives } = this.props
+		return (
+			<div>
+				{representatives.map(function(representative) {
+					if (representative.id === JSON.parse(params.id)) {
+						return <div>{representative.person.firstname}</div>
+					}
+				})}
 			</div>
-			<div className='col-md-2 col-md-offset-1 rep-info'>
-					<div>Phone:</div>
-					<div>{representative.phone}</div>
-			</div>
-			<div className='col-md-2 col-md-offset-1'></div> 
-		</div>
-		
-	</div>
-)
+		)
+	}
+}
 
 export default RepresentativeInfo
 
 function mapStateToProps(state) {
-  console.log(state)
   const representatives = state.Representatives.representatives
-  const isFetching = state.Representatives.isFetching
-  const progress = state.Spinner.progress
-  const representative = state.Representatives.representative
-
   return {
     representatives,
-    isFetching,
-    representative,
-    progress
   }
 }
 
 export default connect(mapStateToProps)(RepresentativeInfo)
+				// <div className='row rep-info-container'>
+				// 		<div className='col-md-2 col-md-offset-2 rep-info'>
+				// 			<div>Website:</div>
+				// 			<a href={representative.website}>{representative.website}</a>
+				// 		</div>
+				// 	<div className='col-md-2 col-md-offset-1 rep-info'>
+				// 			<div>Twitter:</div>
+				// 			<a href={'https://www.twitter.com/@' + representative.person.twitterid}>@{representative.person.twitterid}</a>
+				// 	</div>
+				// 	<div className='col-md-2 col-md-offset-1 rep-info'>
+				// 			<div>Phone:</div>
+				// 			<div>{representative.phone}</div>
+				// 	</div>
+				// 	<div className='col-md-2 col-md-offset-1'></div> 
+				// </div>
