bdbceff90d1597c169f6319ee9e4a86cafe56515
Colin McDonald <mickydwow@gmail.com>
2016-04-05 16:09:14 -0700

    Working on refactoring the routing

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,13 @@ const API_KEY = 'AIzaSyD2uEW__R9AOm1JrooaddNSZM1EdN6KhAc'
 
 
 export default class App extends Component {
-	  componentDidMount() {
-    	window.navigator.geolocation.getCurrentPosition(function(pos){
-      fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + ' &result_type=postal_code&key=' + API_KEY)
-        .then(response => response.json())
-        .then(location => this.props.dispatch(getRepresentatives(location.results[0].address_components[0].short_name)))
-      }.bind(this))
-    }
+  componentDidMount() {
+  	window.navigator.geolocation.getCurrentPosition(function(pos){
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + ' &result_type=postal_code&key=' + API_KEY)
+      .then(response => response.json())
+      .then(location => this.props.dispatch(getRepresentatives(location.results[0].address_components[0].short_name)))
+    }.bind(this))
+  }
 
   render() {
 	const { store, children } = this.props
