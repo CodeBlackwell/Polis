fc35e7ba1313c42cec8e953b48ffbbef14b96dec
DejonGill <dejonjgill@gmail.com>
2016-04-06 18:57:42 -0700

    Added target="_blank" for links in RepDetails.js

--- src/components/RepresentativeDetails.js
+++ src/components/RepresentativeDetails.js
@@ -4,11 +4,11 @@ const RepresentativeDetails = ({representative}) => (
 	<div className='row'>
 			<div className='col-md-2 col-md-offset-2 rep-info'>
 				<div>Website:</div>
-				<a href={representative.website}>{representative.website}</a>
+				<a href={representative.website} target="_blank">{representative.website}</a>
 			</div>
 		<div className='col-md-2 col-md-offset-1 rep-info'>
 				<div>Twitter:</div>
-				<a href={'https://www.twitter.com/@' + representative.person.twitterid}>@{representative.person.twitterid}</a>
+				<a href={'https://www.twitter.com/@' + representative.person.twitterid} target="_blank">@{representative.person.twitterid}</a>
 		</div>
 		<div className='col-md-2 col-md-offset-1 rep-info'>
 				<div>Phone:</div>
