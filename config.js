f6f6cb0d4d64d88896617b6582dc9120419c54fb
colinlmcdonald <mickydwow@gmail.com>
2016-04-15 14:48:26 -0700

    Deploying to heroku

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  dbURI: 'mongodb://Le:Database211@ds035310.mlab.com:35310/heroku_hkr86p3z',
+  dbURI2: 'mongodb://heroku_hkr86p3z:p8b5cs9j209tnhf331u7v969v6@ds035310.mlab.com:35310/heroku_hkr86p3z',
+  secret: 'uihauifhdif1231314114'
+}
