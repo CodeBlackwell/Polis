99ac2feb12415339be8372fecb2acc0534f01302
colinlmcdonald <mickydwow@gmail.com>
2016-04-15 19:57:19 -0700

    Debugging

---
+++ src/components/Todo NOT THE APP.js
@@ -0,0 +1,6 @@
+//TODO: Zipcode entry is overwritten by google maps api
+//TODO: Upcoming rep bills does not register vote (401 unauthorized)
+//TODO: Refresh the app breaks it
+//TODO: Login error messaging
+//TODO: Top navbar styling
+//TODO: Next 10 Words click twice
