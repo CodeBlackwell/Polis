e3d681a9fe8e0ca9c8a65ad3bf596e252b7faa5b
DejonGill <dejonjgill@gmail.com>
2016-04-22 16:08:56 -0700

    Contributor data passed into props

--- src/components/ContributorVisualization.js
+++ src/components/ContributorVisualization.js
@@ -1,4 +1,5 @@
 import d3 from 'd3'
+import c3 from 'c3'
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { connect } from 'react-redux'
@@ -9,43 +10,60 @@ import { setGraphType } from '../actions/actionContributor'
 
 export class ContributorVisualization extends Component {
 
+
     handleChange(value) {
       this.props.dispatch(setGraphType())
     }
 
+    create(data) {
+    const chart  = c3.generate({
+      bindto: '#contribution_chart',
+      data: {
+        columns: ['rjaljflakjf', 1, 4, 52, 6, 7474],
+        types: {
+          data1: 'bar',
+          data2: 'bar',
+          data3: 'bar',
+          data4: 'bar',
+          data5: 'bar',
+          data6: 'bar'
+        }
+      }
+    });
+    
+  }
+
+  componentDidMount() {
+    this.create(this.props.contributions)
+  }
+
     render() {
         const grouped = 'grouped'
         const stacked = 'stacked'
-        return (
-            <div>
-              { this.props.m ? 
-                <div>
-                   <form>
-                      <label><input type="radio" name="mode" value={grouped} onChange={this.handleChange.bind(this, grouped)}/> Grouped</label>
-                      <label><input defaultChecked={true} type="radio" name="mode" value={stacked} onChange={this.handleChange.bind(this, stacked)} /> Stacked</label>
-                   </form>
-                  <Chart
-                      type={'custom'}
-                      customChart={Visualization}
-                      data={this.props} /> 
-                  </div>
-                : <div>Loading...</div> }
-            </div>
-        );
+        const { contributions } = this.props
+        console.log('this.props from contributionVis', this.props)
+        return <div>
+      { this.props.contributions ? 
+        <div id="contribution_chart"></div>
+        : <div>Loading...</div> }
+      </div>
     }
 }
 
 function mapStateToProps(state) {
+  console.log('state from ContributorVisualization', state)
   const m = state.ContributorVisualization.m
   const n = state.ContributorVisualization.n
   const layers = state.ContributorVisualization.layers
   const grouped = state.ContributorVisualization.grouped
-
+  const contributions = state.ContributorVisualization.contributions
+  console.log('this should be contributor data from contributorViz', contributions)
   return {
     m,
     n,
     layers,
-    grouped
+    grouped,
+    contributions
   }
 }
 
